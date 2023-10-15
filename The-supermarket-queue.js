14f329f5bea866156c5b689cfb38c6fcf030cc32
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-15 13:21:53 -0700

    Update The-supermarket-queue.js

--- The-supermarket-queue.js
+++ The-supermarket-queue.js
@@ -49,10 +49,25 @@ function queueTime(customers, registers) {
 // or
 
 function queueTime(customers, n) {
-  let w = new Array(n).fill(0);
-  for (let t of customers) {
-    let idx = w.indexOf(Math.min(...w));
-    w[idx] += t;
+
+  // Make an array containing n amount of elements, 
+  // n representing the number of tills, 
+  // and fill with zeroes
+  let tills = new Array(n).fill(0);
+
+  // Iterate through the customers 
+  for (let time of customers) {
+
+    // Find the till that has the least time
+    let idx = tills.indexOf(Math.min(...tills));
+
+    // Add the time, representing the customer to
+    // the till that has the least time
+    tills[idx] += time;
   }
-  return Math.max(...w);
+
+  // Return the till that has the longest wait time
+  // This is the required time to get all customers
+  // through the que.
+  return Math.max(...tills);
 }
